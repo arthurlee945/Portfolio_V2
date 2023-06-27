9a947593f0f0edb0765c4467590a1d23e00013c0
Arthur Lee <arthur.lee945@gmail.com>
2023-06-27 18:36:06 -0500

    ud

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,26 @@ import "../styles/font-face.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/Layout";
 import GlobalContextProvider from "utils/GlobalContext";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <GlobalContextProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </GlobalContextProvider>
-    </>
-  );
+    return (
+        <>
+            <Script src="https://www.googletagmanager.com/gtag/js?id=G-X65RVRKX9Z" />
+            <Script id="google-analytics">
+                {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-X65RVRKX9Z');
+                `}
+            </Script>
+            <GlobalContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </GlobalContextProvider>
+        </>
+    );
 }
